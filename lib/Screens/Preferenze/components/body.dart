3306c8bec56795d2c707739361196b4fcddec7db
f.aurilio <f.aurilio@studenti.unisa.it>
2021-02-04 13:09:44 +0100

    Fine implementazione notifiche di pranzo e cena in base all'orario indicato dall'utente. Modifica del form che permette all'utente di inserire gli orari abituali di pranzo e cena con l'aggiunta di un widget TimePicker utile per far scegliere all'utente gli orari.

--- lib/Screens/Preferenze/components/body.dart
+++ lib/Screens/Preferenze/components/body.dart
@@ -4,10 +4,10 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:smart_cupboard/Screens/HomePage/home_page.dart';
 import 'package:smart_cupboard/Screens/Login/components/background.dart';
 import 'package:smart_cupboard/Screens/RicercaRicette/ricerca_ricette.dart';
-import 'package:smart_cupboard/Screens/Ricetta/ricetta_screen.dart';
 import 'package:smart_cupboard/components/rounded_button.dart';
 import 'package:smart_cupboard/components/rounded_input_field.dart';
-
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 class Body extends StatefulWidget {
 
@@ -19,19 +19,58 @@ class _BodyState extends State<Body> {
 
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey(debugLabel: "Main Navigator");
 
+  TimeOfDay _timeMattina,_timeSera;
+
   @override
   void initState() {
-    super.initState();
+    tz.initializeTimeZones();
     var androidInitialize = new AndroidInitializationSettings("mipmap/ic_launcher");
 
     var IOSInitialize = new IOSInitializationSettings();
     var initializationSettings = new InitializationSettings(android: androidInitialize, iOS: IOSInitialize);
 
     localNotification = new FlutterLocalNotificationsPlugin();
+
+    _timeMattina = TimeOfDay.now();
+    _timeSera = TimeOfDay.now();
+
     try {
       localNotification.initialize(initializationSettings, onSelectNotification: selectNotification );
+
+
     }on MissingPluginException catch (e){
       print(e.message);
+    }
+
+
+    super.initState();
+  }
+
+  _pickerTimeMattina() async{
+    TimeOfDay timeMattina = await showTimePicker(context: context, initialTime: _timeMattina,
+        builder: (BuildContext context, Widget child){
+          return Theme(data: ThemeData(),child: child,);
+        }
+    );
+
+    if(timeMattina != null){
+      setState(() {
+        _timeMattina = timeMattina;
+      });
+    }
+  }
+
+  _pickerTimeSera() async{
+    TimeOfDay timeSera = await showTimePicker(context: context, initialTime: _timeSera,
+        builder: (BuildContext context, Widget child){
+          return Theme(data: ThemeData(),child: child,);
+        }
+    );
+
+    if(timeSera != null){
+      setState(() {
+        _timeSera = timeSera;
+      });
     }
   }
 
@@ -45,14 +84,21 @@ class _BodyState extends State<Body> {
     );
   }
 
-  Future showNotification() async{
+  Future showNotification(TimeOfDay mattina, TimeOfDay sera) async{
     var andriodDetaild = new AndroidNotificationDetails("channelId", "LocalNotification", " FUNZIONERà????",importance: Importance.high);
 
     var iOSDetails = new IOSNotificationDetails();
 
     var generalNotificationDetails = new NotificationDetails(android: andriodDetaild, iOS: iOSDetails);
 
-    await localNotification.show(0, "Asfenazza", "body della notifica", generalNotificationDetails);
+
+    final now = new DateTime.now();
+    DateTime d =  new DateTime(now.year, now.month, now.day, _timeMattina.hour, _timeMattina.minute);
+
+    DateTime d1 =  new DateTime(now.year, now.month, now.day, _timeSera.hour, _timeSera.minute);
+    await localNotification.zonedSchedule(0, "SmartCupboard: Pranzo", "Ehi... è ora di pranzare accedi per visualizzare le ricette che puoi fare.", tz.TZDateTime.from(d, tz.local).add(Duration (seconds: 5)), generalNotificationDetails, uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime, androidAllowWhileIdle: true);
+    await localNotification.zonedSchedule(1, "SmartCupboard: Cena", "Ehi... è ora di cenare accedi per visualizzare le ricette che puoi fare.", tz.TZDateTime.from(d1, tz.local).add(Duration (seconds: 5)), generalNotificationDetails, uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime, androidAllowWhileIdle: true);
+
   }
 
   @override
@@ -75,30 +121,37 @@ class _BodyState extends State<Body> {
               "A che ora pranzi solitamente?",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            RoundedInputField(
-              hintText: "inserisci orario",
-              onChanged: (value) {},
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListTile(
+                title: Text('Orario selezionato:\t\t\t\t\t ${_timeMattina.hour}:${_timeMattina.minute}'),
+                trailing: Icon(Icons.timer),
+                onTap: _pickerTimeMattina,
+              ),
             ),
             Text(
               "A che ora ceni solitamente?",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            RoundedInputField(
-              hintText: "inserisci orario",
-              onChanged: (value) {},
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListTile(
+                title: Text('Orario selezionato:\t\t\t\t\t ${_timeSera.hour}:${_timeSera.minute}'),
+                trailing: Icon(Icons.timer),
+                onTap: _pickerTimeSera,
+              ),
             ),
             RoundedButton(
               text: "Invia",
               press: () {
-                showNotification();
-                /*Navigator.push(
+                showNotification(_timeMattina,_timeSera).then((value) => Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) {
                       return HomePage();
                     },
                   ),
-                );*/
+                ));
               },
             ),
             SizedBox(height: size.height * 0.03),
